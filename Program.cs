cd50936ed7a846152118dd7aba6d98f3710cb6dc
ImranSultani <ac158276@avcol.school.nz>
2026-05-27 11:59:22 +1200

    Revert "Created an array of 5 easy Maori Quiz questions."
    This reverts commit fa63dab05c05e5168f3ccaaa4e078ccee8ccf1d2.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,2 @@
-﻿namespace MaoriQuizqqeqe
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            String[] easyquestions = { "1. What is the correct Māori word for \"water\"?\nA) Wai\nB) Kai\nC) Rā\nD) Ika", "2. If someone says \"Kia ora\", what is the best English translation?\nA) Good morning\nB) Hello / Cheers\nC) Goodbye\nD) Thank you", "3. What does the word \"Kai\" mean in Māori?\nA) To sleep\nB) To run\nC) Food\nD) Dog", "4. How do you say \"Goodbye\" to someone who is staying (when you are the one leaving)?\nA) Haere rā\nB) E noho rā\nC) Kia ora\nD) Mōrena", "5. Which of these numbers means \"Five\"?\nA) Tahi\nB) Waru\nC) Rima\nD) Tekau" };
-
-            Console.WriteLine("################################\nWelcome To The Maori Quiz!\n");
-            Console.WriteLine("################################\nWhat's your name?");
-            string nameEntered = Console.ReadLine();
-
-            Console.WriteLine("\n################################\nWhat difficulty do you want to select?\n Easy(e) - Medium(m) - Hard(h)");
-            char diffucultySelector = Convert.ToChar(Console.ReadLine());
-            switch
-        }
-    }
-}
+﻿Console.WriteLine("Welcome To The Quiz, What Is Your Name?");
+string nameOfUser = Console.ReadLine();
